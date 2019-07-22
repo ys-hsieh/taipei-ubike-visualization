cdad4de5fa116c4f9f337e1128f6bb64a4a88612
Yuan-Shen, Hsieh <alberthsies@gmail.com>
2019-07-22 12:15:07 +0800

    Add testing view

--- ubike/vis/views.py
+++ ubike/vis/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+globalCounter = 0
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the ubike/vis index.")
+
+def counter(request):
+    return HttpResponse("The cron.yaml has already call this function for " + str(globalCounter) + " times")
+
+def count(request):
+    global globalCounter
+    globalCounter += 1
+    return HttpResponse("added")
