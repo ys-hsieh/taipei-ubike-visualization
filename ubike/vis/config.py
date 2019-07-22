19ae516324cf282836e5df3a882b3c052b707b52
Yuan-Shen, Hsieh <alberthsies@gmail.com>
2019-07-22 11:55:06 +0800

    Listed Ubike API urls

---
+++ ubike/vis/config.py
@@ -0,0 +1,5 @@
+# YouBike API URL
+TaipeiYouBikeAPI = "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.gz"
+NewTaipeiCityYouBikeAPI = "https://data.ntpc.gov.tw/od/data/api/54DDDC93-589C-4858-9C95-18B2046CC1FC?$format=json"
+TaoyuanYouBikeAPI = "https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=5ca2bfc7-9ace-4719-88ae-4034b9a5a55c&rid=a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f"
+
